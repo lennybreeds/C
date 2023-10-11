9ca73196f08a6ff268c7dad0ec69c4781a24cf6e
lennybreeds <146842461+lennybreeds@users.noreply.github.com>
2023-10-11 13:50:55 +0000

    Factorial added

--- week1/factorial.c
+++ week1/factorial.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int factorial (int num){
-    if (num > 0){
-        return factorial(num-1)*num;
+long long int factorial (long long int num){
+    long long int total = 1;
+    for (int l = 1; l<=num;l++){
+        total = total*l;
     }
+    return total;
 }
 
 int main(){
-    int num;
-    scanf("%d",&num);
-    num = factorial(num);
-    printf("%d",num);
+    long long int num;
+    for (int i = 0; i<30;i++) {
+    num = factorial(i);
+    printf("The factorial of %d is %llu\n",i,num);
+    };
+
+    return 0;
 }
